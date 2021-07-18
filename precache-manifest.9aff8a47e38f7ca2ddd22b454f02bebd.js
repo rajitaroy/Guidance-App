c2aee6115be1f7ed0a927acbc66fb468f7e82caa
Rajita Roy <royrajita6@gmail.com>
2021-07-18 23:42:49 +0530

    Updates

---
+++ precache-manifest.9aff8a47e38f7ca2ddd22b454f02bebd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c2e568570a4aa62376477386054eb5",
+    "url": "/Guidance-App/index.html"
+  },
+  {
+    "revision": "3784136c8d3f7901ad49",
+    "url": "/Guidance-App/static/css/main.b7e98b2e.chunk.css"
+  },
+  {
+    "revision": "be8764e90a80b8a08290",
+    "url": "/Guidance-App/static/js/2.bf44d81a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Guidance-App/static/js/2.bf44d81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3784136c8d3f7901ad49",
+    "url": "/Guidance-App/static/js/main.7078d7f5.chunk.js"
+  },
+  {
+    "revision": "8d16fd9b34d1bac2f21e",
+    "url": "/Guidance-App/static/js/runtime-main.75f51a21.js"
+  },
+  {
+    "revision": "5909bf676e137c132176cd8e5e761300",
+    "url": "/Guidance-App/static/media/city.5909bf67.jpg"
+  }
+]);
